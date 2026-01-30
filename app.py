dcf68f654c2a5b541e0951b5c50f8942c93bcbd4
Yeibert <yeibertveliz10@gmail.com>
2026-01-29 21:55:33 -0400

    Pagina Completa

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from pymongo import MongoClient
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from itsdangerous import URLSafeTimedSerializer as Serializer
+from dotenv import load_dotenv
 from functools import wraps
 
+load_dotenv()
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-app.secret_key = "holahola"
+app.secret_key = os.getenv("SECRET_KEY")
 
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client['db1'] 
